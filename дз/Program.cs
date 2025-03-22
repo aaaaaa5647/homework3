56dd37ade6e37d0b921adfe5e5f1a8184c1f6591
aaaaaa5647 <nominaalexeeva@gmail.com>
2025-03-22 17:59:37 +0800

    дз

---
+++ дз/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace дз
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task2();
+            Task3();
+        }
+        static void Task2()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a * b / 100);
+        }
+        static void Task3()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a * 1000 + b * 100 + c * 10 + d);
+        }
+    }
+}
